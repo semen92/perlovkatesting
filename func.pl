981f6f45a7579f84d418c39ab2ee64c897da72b9
semen92 <a.semenov2305@mail.ru>
2022-10-15 21:51:27 +0300

    FAIL FUNCTION
    FAIL ADD FUN

---
+++ func.pl
@@ -0,0 +1,24 @@
+use strict;
+print('enter Y');
+  my $Y=<STDIN>;
+  print ('enter x'); 
+  my $x=<STDIN>;
+  print ('enter z');
+  my $z=<STDIN>>
+  sub maxandnin($y,$x,$z){
+    maxv=undef
+   our @arr=@_;
+  for(@arr){
+our $minv = $_ if !$minv || $_ < $minv;
+$maxv = $_ if !$maxv || $_ > $maxv
+  } 
+  
+  
+ return $minv,$maxv
+ }
+ &maxandnin($y,$X,$Z);
+
+ 
+ 
+ 
+ 
